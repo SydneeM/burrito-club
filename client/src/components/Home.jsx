3c360750d275b83567c709d8665b44afe9d778fa
Sydnee Mizuno <syd.mizuno@gmail.com>
2025-02-18 19:22:04 -0700

    Install headless ui

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,15 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router'
+import { Button, Input, Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
+
+const rooms = [
+  { id: 1, name: 'Burrito' },
+  { id: 2, name: 'Other' }
+]
 
 function Home() {
   const [username, setUsername] = useState('');
+  const [room, setRoom] = useState(rooms[0]);
   let navigate = useNavigate();
 
   const handleNameChange = (value) => {
@@ -13,12 +20,22 @@ function Home() {
   return (
     <div className='flex flex-col gap-y-4'>
       <h1 className='p-4'>Join the Club</h1>
-      <input
+      <Input
         className='bg-[#1a1a1a] p-3 rounded-md'
         placeholder='Username'
         onChange={(e) => handleNameChange(e.target.value)}
       />
-      <button onClick={() => navigate('/about')}>Enter</button>
+      <Listbox value={room} onChange={setRoom}>
+        <ListboxButton>{room.name}</ListboxButton>
+        <ListboxOptions anchor="bottom">
+          {rooms.map((room) => (
+            <ListboxOption key={room.id} value={room}>
+              {room.name}
+            </ListboxOption>
+          ))}
+        </ListboxOptions>
+      </Listbox>
+      <Button onClick={() => navigate('/about')}>Enter</Button>
     </div >
   )
 }
