10ed097b310aab62eff76ce804ab88e93aea6588
Sydnee Mizuno <syd.mizuno@gmail.com>
2025-02-20 19:41:42 -0700

    Adjust styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router'
-import { Button, Input, Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
 
 const rooms = [
   { id: 1, name: 'Burrito' },
@@ -13,32 +13,36 @@ function Home({ socket }) {
   let navigate = useNavigate();
 
   return (
-    <div className='flex flex-col gap-y-4'>
-      <h1 className='p-4'>Join the Club</h1>
-      <Input
-        className='bg-[#1a1a1a] p-3 rounded-md'
-        placeholder='Username'
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <Listbox value={room} onChange={setRoom} >
-        <ListboxButton>{room.name}</ListboxButton>
-        <ListboxOptions anchor="bottom">
-          {rooms.map((room) => (
-            <ListboxOption key={room.id} value={room}>
-              {room.name}
-            </ListboxOption>
-          ))}
-        </ListboxOptions>
-      </Listbox>
-      <Button onClick={() => {
-        if (username !== '' && username.trim().length !== 0 && room.name !== '') {
-          const state = { username, room: room.name };
-          socket.emit('join_room', state);
-          navigate('/room', { state });
-        }
-      }}>
-        Enter
-      </Button>
+    <div className='flex h-screen ring-1'>
+      <div className='flex flex-col gap-y-4 m-auto'>
+        <h1 className=''>Join the Club</h1>
+        <input
+          className='ring-1'
+          placeholder='Username'
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Listbox value={room} onChange={setRoom}>
+          <ListboxButton className='ring-1'>{room.name}</ListboxButton>
+          <ListboxOptions anchor="bottom">
+            {rooms.map((room) => (
+              <ListboxOption key={room.id} value={room}>
+                {room.name}
+              </ListboxOption>
+            ))}
+          </ListboxOptions>
+        </Listbox>
+        <button
+          className='ring-1'
+          onClick={() => {
+            if (username !== '' && username.trim().length !== 0 && room.name !== '') {
+              const state = { username, room: room.name };
+              socket.emit('join_room', state);
+              navigate('/room', { state });
+            }
+          }}>
+          Enter
+        </button>
+      </div>
     </div >
   );
 }
