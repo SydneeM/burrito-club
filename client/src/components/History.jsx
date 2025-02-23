d6ec04aab201574cb6788a67f8ba8e20db123f6b
Sydnee Mizuno <syd.mizuno@gmail.com>
2025-02-22 23:37:15 -0700

    Start arranging history entries

--- client/src/components/History.jsx
+++ client/src/components/History.jsx
@@ -2,12 +2,17 @@ function History({ restaurants }) {
   return (
     <div className='flex flex-col h-1/2 overflow-scroll rounded-3xl m-4' id='history'>
       <h2 className='text-left font-semibold text-2xl m-4'>Restaurant History</h2>
+      <div className='text-left flex flex-row overflow-x-scroll mx-2'>
+        <span className='w-1/3 mx-2'>Restaurant</span>
+        <span className='w-1/3 mx-2'>Buyer</span>
+        <span className='w-1/3 mx-2'>Date</span>
+      </div>
       <ul className=''>
         {restaurants.map((restaurant) => (
-          <li key={`${restaurant.name}-${restaurant.time}`} className='text-left'>
-            <span>{restaurant.name}</span>
-            <span>{restaurant.buyer}</span>
-            <span>{restaurant.time}</span>
+          <li key={`${restaurant.name}-${restaurant.time}`} className='text-left flex flex-row overflow-x-scroll mx-2'>
+            <span className='w-1/3 mx-2'>{restaurant.name}</span>
+            <span className='w-1/3 mx-2'>{restaurant.buyer}</span>
+            <span className='w-1/3 mx-2'>{new Date(restaurant.time).toLocaleDateString()}</span>
           </li>
         ))}
       </ul>
