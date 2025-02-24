e58a256208c431d6b55e77ddb4bef86c74f4cd9b
Sydnee Mizuno <smizuno@nulite.lighting>
2025-02-24 16:30:57 -0700

    Fix scroll

--- client/src/components/History.jsx
+++ client/src/components/History.jsx
@@ -1,8 +1,8 @@
 function History({ restaurants }) {
   return (
-    <div className='md:h-1/3 card overflow-y-scroll'>
+    <div className='md:h-1/3 card'>
       <h2 className=''>Restaurant History</h2>
-      <div className="flex flex-col">
+      <div className="flex flex-col overflow-y-scroll">
         <div className='grid grid-cols-3 text-start'>
           <span className='font-semibold text-lg'>Restaurant</span>
           <span className='font-semibold text-lg'>Buyer</span>
