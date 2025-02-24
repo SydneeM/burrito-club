79af6e03174dce9e0bbd810d728f444b029d3b0c
Sydnee Mizuno <smizuno@nulite.lighting>
2025-02-24 12:25:27 -0700

    Adjust text in Choice

--- client/src/components/Choice.jsx
+++ client/src/components/Choice.jsx
@@ -4,8 +4,14 @@ const Choice = memo(function Choice({ restaurant, buyer }) {
   return (
     <div className='md:h-1/3 card'>
       <h2 className=''>Restaurant of the Week</h2>
-      <p className='mx-4'>Restaurant: {restaurant}</p>
-      <p className='mx-4'>Who&apos;s Paying: {buyer}</p>
+      <div className='flex flex-row items-baseline'>
+        <span className='ml-4 font-semibold text-lg'>Restaurant:</span>
+        <span className='ml-1'>{restaurant}</span>
+      </div>
+      <div className='flex flex-row items-baseline'>
+        <h3 className='ml-4 font-semibold text-lg'>Who&apos;s Paying:</h3>
+        <h3 className='ml-1'>{buyer}</h3>
+      </div>
     </div>
   );
 });
