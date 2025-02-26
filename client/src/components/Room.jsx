0d87c5188d61b5c2949d9187b91afd8470553b01
Sydnee Mizuno <syd.mizuno@gmail.com>
2025-02-25 23:04:44 -0700

    Format doc to remove extra spaces

--- client/src/components/Room.jsx
+++ client/src/components/Room.jsx
@@ -56,57 +56,57 @@ function Room({ socket }) {
   }, [socket]);
 
   return (
-      <div className='flex flex-col md:flex-row md:gap-x-10 gap-y-4 h-screen'>
+    <div className='flex flex-col md:flex-row md:gap-x-10 gap-y-4 h-screen'>
 
-        <div className='flex flex-col'>
-          <TabGroup className='h-full'>
-            <div className='flex flex-col md:flex-row md:gap-x-10 gap-y-4 h-full'>
-              <TabList className='flex flex-row md:flex-col gap-x-4 md:gap-y-4 p-4 md:px-10 border-b-1 md:border-r-1 border-gray-700'>
-                <div className='items-center'>{`${room} Club`}</div>
-                <div className='items-center'>{`Hello ${username}`}</div>
-                <Tab className='flex flex-row gap-x-2 items-center'>
-                  <StarIcon className='size-8 text-blue-500' />
-                  <span className='hidden sm:block text-nowrap'>Restaurant of the Week</span>
-                </Tab>
-                <Tab className='flex flex-row gap-x-2 items-center'>
-                  <ClockIcon className='size-8 text-blue-500' />
-                  <span className='hidden sm:block text-nowrap'>Restaurant History</span>
-                </Tab>
-                <Tab className='flex flex-row gap-x-2 items-center'>
-                  <UsersIcon className='size-8 text-blue-500' />
-                  <span className='hidden sm:block text-nowrap'>Current Members</span>
-                </Tab>
-              </TabList>
-              <div className='flex flex-col md:w-[30vw] md:my-10 gap-y-4 md:gap-y-10 w-full'>
-                <TabPanels className='p-4 bg-[#282b31] rounded-3xl md:grow'>
-                  <TabPanel>
-                    <Choice restaurant={restaurant} buyer={buyer} />
-                  </TabPanel>
-                  <TabPanel>
-                    <History restaurants={restaurantHistory} />
-                  </TabPanel>
-                  <TabPanel>
-                    <Users socket={socket} curRoom={room} curUser={username} users={roomUsers} />
-                  </TabPanel>
-                </TabPanels>
-                <div className='p-4 bg-[#282b31] rounded-3xl md:grow'>
-                  <h3>Restaurant Selection</h3>
-                  <ChoiceSender socket={socket} curRoom={room} />
-                </div>
+      <div className='flex flex-col'>
+        <TabGroup className='h-full'>
+          <div className='flex flex-col md:flex-row md:gap-x-10 gap-y-4 h-full'>
+            <TabList className='flex flex-row md:flex-col gap-x-4 md:gap-y-4 p-4 md:px-10 border-b-1 md:border-r-1 border-gray-700'>
+              <div className='items-center'>{`${room} Club`}</div>
+              <div className='items-center'>{`Hello ${username}`}</div>
+              <Tab className='flex flex-row gap-x-2 items-center'>
+                <StarIcon className='size-8 text-blue-500' />
+                <span className='hidden sm:block text-nowrap'>Restaurant of the Week</span>
+              </Tab>
+              <Tab className='flex flex-row gap-x-2 items-center'>
+                <ClockIcon className='size-8 text-blue-500' />
+                <span className='hidden sm:block text-nowrap'>Restaurant History</span>
+              </Tab>
+              <Tab className='flex flex-row gap-x-2 items-center'>
+                <UsersIcon className='size-8 text-blue-500' />
+                <span className='hidden sm:block text-nowrap'>Current Members</span>
+              </Tab>
+            </TabList>
+            <div className='flex flex-col md:w-[30vw] md:my-10 gap-y-4 md:gap-y-10 w-full'>
+              <TabPanels className='p-4 bg-[#282b31] rounded-3xl md:grow'>
+                <TabPanel>
+                  <Choice restaurant={restaurant} buyer={buyer} />
+                </TabPanel>
+                <TabPanel>
+                  <History restaurants={restaurantHistory} />
+                </TabPanel>
+                <TabPanel>
+                  <Users socket={socket} curRoom={room} curUser={username} users={roomUsers} />
+                </TabPanel>
+              </TabPanels>
+              <div className='p-4 bg-[#282b31] rounded-3xl md:grow'>
+                <h3>Restaurant Selection</h3>
+                <ChoiceSender socket={socket} curRoom={room} />
               </div>
             </div>
-          </TabGroup>
-        </div>
-
-        <div className='flex flex-col grow bg-[#282b31] rounded-3xl md:my-10 md:mr-10'>
-          {/* <div className='p-2'>{`${room} Chat`}</div> */}
-          <div className='flex flex-col h-full overflow-auto'>
-            <Messages messages={roomMessages} curUser={username} />
-            <MessageSender socket={socket} curRoom={room} curUser={username} />
           </div>
-        </div>
-
+        </TabGroup>
       </div>
+
+      <div className='flex flex-col grow bg-[#282b31] rounded-3xl md:my-10 md:mr-10'>
+        {/* <div className='p-2'>{`${room} Chat`}</div> */}
+        <div className='flex flex-col h-full overflow-auto'>
+          <Messages messages={roomMessages} curUser={username} />
+          <MessageSender socket={socket} curRoom={room} curUser={username} />
+        </div>
+      </div>
+
+    </div>
   );
 }
 
